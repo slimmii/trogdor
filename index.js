569f8427e4f1302d317606dc2ea07817f7e54e96
Andie Similon <andie@appstrakt.com>
2015-05-29 13:09:50 +0200

    wiii

--- index.js
+++ index.js
@@ -6,35 +6,63 @@ var slack = new Slack('https://hooks.slack.com/services/T04N3PW6G/B053V2J6X/cZ31
 })
 var express = require('express');
 var bodyParser = require('body-parser');
+var htmlToText = require('html-to-text');
+var requestLib = require('request');
+var cheerio = require('cheerio');
 var app = express();
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({
+	extended: false
+})
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.post('/wikiwiki', urlencodedParser, function(request, response) {
-	messages = {
-	  text: "WINAK WikiWiki",
-	  channel: "#neejberhood",
-	  attachments: [
-	    {
-	      fallback: request.body.text + " [ <http://wikiwiki.winak.be/index.php/" + request.body.text + "|" + request.body.text + ">]",
-	      color: "#36a64f", // Can either be one of 'good', 'warning', 'danger'
-		  thumb_url : "http://www.winak.be/sites/default/files/WINAK%20Schild%20ZW-OR.png",
-	      fields: [
-	        {
-	          title: request.body.text,
-	          value: "This wiki wiki page is pretty awesome <http://wikiwiki.winak.be/index.php/" + request.body.text + "|" + request.body.text + ">",
-	          short: false 
-	        }
-	      ]
-	    }
-	  ]
-	};
-	slack.notify(messages);
+app.post('/wikiwiki', urlencodedParser, function(req, res) {
+	
+	//slack.notify(messages);
+	requestLib.get('http://wikiwiki.winak.be/index.php/' + req.body.text, function(error, response, body) {
+		if (!error && response.statusCode == 200) {
+		    $ = cheerio.load(body);
+			var text = htmlToText.fromString($('#bodyContent').html(), {
+			});
+			console.log(text);
+			//slack.notify(text);
+			
+			var html = text.replace(
+    // Replace out the new line character.
+    new RegExp( "\\n--------------------------------------------------------------------------------\\n", "g" ), 
+    
+    // Put in ... so we can see a visual representation of where
+    // the new line characters were replaced out.
+    "" 
+    );
+			
+			messages = {
+				text: "WINAK WikiWiki",
+				channel: "#neejberhood",
+				attachments: [{
+					fallback: req.body.html + " [ <http://wikiwiki.winak.be/index.php/" + req.body.text + "|" + req.body.text + ">]",
+					color: "#36a64f", // Can either be one of 'good', 'warning', 'danger'
+					thumb_url: "http://www.winak.be/sites/default/files/WINAK%20Schild%20ZW-OR.png",
+					text: html
+				}]
+			};
+			
+			slack.notify(messages);
+		}
+		res.send('');
+	});
 
-	response.send('');
+	// function(error, result) {
+	// 		console.log(result.body);
+	// 		var text = htmlToText.fromString(result.body, {
+	// 			wordwrap: 130
+	// 		});
+	// 		console.log(text);
+	// 		response.send('test');
+	// 	});
+
 });
 
 app.listen(app.get('port'), function() {
