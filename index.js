3668756b4bd080de25daac03f2b3659cba2b5e1d
Andie Similon <andie@appstrakt.com>
2015-05-29 10:03:30 +0200

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
+app.post('/wikiwiki', function(request, response) {
   response.send('Herro World!');
 });
 
