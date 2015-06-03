638c5e9503d93da7d5417750fd93a5fa231e8a75
Andie Similon <andie@appstrakt.com>
2015-06-03 14:42:34 +0200

    lastquote

--- index.js
+++ index.js
@@ -154,6 +154,27 @@ app.post('/wikiwiki', urlencodedParser, function(req, res) {
 
 });
 
+app.post('/lastquote', urlencodedParser, function(req, res) {
+
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		client.query('select * from quotes order by id desc limit 1;', function(err, result) {
+			done();
+			if (err) {
+				console.error(err);
+				res.send("Error " + err);
+			} else {
+				if (result.rows.length > 0) {
+					slack.notify(result.rows[0].quote);
+					res.send("");
+				} else {
+					res.send("There were no quotes available");
+				}
+			}
+		});
+	});
+
+}
+
 app.post('/quote', urlencodedParser, function(req, res) {
 	var id = req.body.text;
 
