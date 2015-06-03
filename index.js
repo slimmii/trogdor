25464e6d49c6a9e45c0f11805c32d951ce9930ed
Andie Similon <andie@appstrakt.com>
2015-06-03 13:49:30 +0200

    quotes

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ app.post('/wikiwiki', urlencodedParser, function(req, res) {
 
 app.post('/quote', urlencodedParser, function(req, res) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('SELECT * FROM test_table', function(err, result) {
+		client.query('SELECT * FROM quotes', function(err, result) {
 			done();
 			if (err) {
 				console.error(err);
