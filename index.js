3daca9b0bdd22d2232247240da6f032e9af62678
Tom Roels <tom.roels94@gmail.com>
2015-06-11 16:29:14 +0200

    update

--- index.js
+++ index.js
@@ -57,8 +57,12 @@ app.post('/slap', urlencodedParser, function(req, res) {
 				res.send(err);
 			} else {
 				if (result.rows.length > 0) {
+					var random_int = randomInt(0, result.rows.length);
+					var slap_message = result.rows[random_int];
+					slap_message = slap_message.replace("name1", req.body.user_name);
+					slap_message = slap_message.replace("name2", req.body.text);
 					messages = {
-						text: result.rows[0].slap,
+						text: slap_message,
 						channel: "#" + req.body.channel_name
 					};
 					slack.notify(messages);
