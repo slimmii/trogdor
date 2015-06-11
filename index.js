60ed0073b80c06f78c289b7caec1882491496076
Tom Roels <tom.roels94@gmail.com>
2015-06-11 14:32:52 +0200

    update

--- index.js
+++ index.js
@@ -57,13 +57,15 @@ app.post('/slap', urlencodedParser, function(req, res) {
 			});
 
 		});
-
+	/*
 	var random_int = randomInt(0, slap_variations.length);
 
 	var slap_message = slap_variations[random_int].replace("name1", req.body.user_name);
 	slap_message = slap_message.replace("name2", req.body.text);
+	*/
 	messages = {
-		text: "*" + slap_message + "*",
+		text: slap_variations[0],
+		//text: "*" + slap_message + "*",
 		channel: "#" + req.body.channel_name
 	};
 	slack.notify(messages);
